24066336e3b15a8ef5fe91e46ad81fd2b8b1f6e5
蔡羽宣 <3a832055@gm.student.ncut.edu.tw>
2021-12-29 22:14:18 +0800

    刪除多筆資料(destroy方法)

--- routes/web.php
+++ routes/web.php
@@ -50,7 +50,9 @@ Route::get('/', function () {
     //$post = Post::find(1);
     //$post->delete();
 
-    Post::destroy(2);
+    //Post::destroy(2);
+
+    Post::destroy(3, 5, 7);
 });
 
 Route::get('posts', [\App\Http\Controllers\PostsController::class, 'index'])->name('posts.index');

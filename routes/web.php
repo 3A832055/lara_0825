ddbce4e9706792314a031678b5070af4708e7a6a
蔡羽宣 <3a832055@gm.student.ncut.edu.tw>
2021-12-29 22:01:47 +0800

    查詢資料(where, get, orderby(desc)方法)

--- routes/web.php
+++ routes/web.php
@@ -30,8 +30,11 @@ Route::get('/', function () {
     //$posts = Post::all();
     //dd($posts);
 
-    $post = Post::find(1);
-    dd($post);
+    //$post = Post::find(1);
+    //dd($post);
+
+    $posts = Post::where('id', '<', 10)->orderBy('id', 'DESC')->get();
+    dd($posts);
 });
 
 Route::get('posts', [\App\Http\Controllers\PostsController::class, 'index'])->name('posts.index');
